d083db7c5256dc272ef762da3201a72310ccc760
mcitem <mcitem@mcitem.com>
2025-07-13 04:53:22 +0800

    chore(tests): deduplicate TestAuthLayer implementation

--- tests/test_middleware.rs
+++ tests/test_middleware.rs
@@ -1,72 +1,7 @@
-use axum::{response::Response, routing::get, BoxError, Router};
-use axum_casbin::{CasbinAxumLayer, CasbinVals};
-use axum_test::TestServer;
-use bytes::Bytes;
+use common::*;
+mod common;
+
 use casbin::function_map::key_match2;
-use casbin::{CoreApi, DefaultModel, FileAdapter};
-use futures::future::BoxFuture;
-use http::{Request, StatusCode};
-use http_body::Body as HttpBody;
-use std::{
-    convert::Infallible,
-    task::{Context, Poll},
-};
-use tower::{Layer, Service};
-
-#[derive(Clone)]
-struct FakeAuthLayer;
-
-impl<S> Layer<S> for FakeAuthLayer {
-    type Service = FakeAuthMiddleware<S>;
-
-    fn layer(&self, inner: S) -> Self::Service {
-        FakeAuthMiddleware { inner }
-    }
-}
-
-#[derive(Clone)]
-struct FakeAuthMiddleware<S> {
-    inner: S,
-}
-
-impl<S, ReqBody, ResBody> Service<Request<ReqBody>> for FakeAuthMiddleware<S>
-where
-    S: Service<Request<ReqBody>, Response = Response<ResBody>, Error = Infallible>
-        + Clone
-        + Send
-        + 'static,
-    S::Future: Send + 'static,
-    ReqBody: Send + 'static,
-    Infallible: From<<S as Service<Request<ReqBody>>>::Error>,
-    ResBody: HttpBody<Data = Bytes> + Send + 'static,
-    ResBody::Error: Into<BoxError>,
-{
-    type Response = S::Response;
-    type Error = S::Error;
-    // `BoxFuture` is a type alias for `Pin<Box<dyn Future + Send + 'a>>`
-    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
-
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
-    }
-
-    fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
-        let not_ready_inner = self.inner.clone();
-        let mut inner = std::mem::replace(&mut self.inner, not_ready_inner);
-
-        Box::pin(async move {
-            let vals = CasbinVals {
-                subject: String::from("alice"),
-                domain: None,
-            };
-            req.extensions_mut().insert(vals);
-            inner.call(req).await
-        })
-    }
-}
-
-// Handler that immediately returns an empty `200 OK` response.
-async fn handler() {}
 
 #[cfg_attr(feature = "runtime-tokio", tokio::test)]
 #[cfg_attr(feature = "runtime-async-std", async_std::test)]
@@ -91,7 +26,10 @@ async fn test_middleware() {
         .route("/pen/2", get(handler))
         .route("/book/{id}", get(handler))
         .layer(casbin_middleware)
-        .layer(FakeAuthLayer);
+        .layer(TestAuthLayer(CasbinVals {
+            subject: String::from("alice"),
+            domain: None,
+        }));
 
     let client = TestServer::new(app).unwrap();
 
